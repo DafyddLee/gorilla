0fa6ce48f2373264d2b3d4e352f76beb7b43e6e7
DafyddLee <166951014+DafyddLee@users.noreply.github.com>
2024-05-23 09:44:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import selenium
+import selenium.webdriver
 import streamlit as st
-
+import time 
 st.title('Gorilla')
+
+driver = selenium.webdriver.Chrome()
+            
+# Navigate to the website
+website = 'https://trakcarelabwebview.nhls.ac.za/trakcarelab/csp/system.Home.cls#/Component/SSUser.Logon'
+driver.get(website)
+
+time.sleep(5)
+driver.quit()
